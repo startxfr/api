615be76c41f1f2c5dbb25c01118d6cb8147ddba6
cl <cl@06c77f61-839e-4d04-8f56-14a2b81da803>
2010-05-13 00:03:42 +0000

    Zunodev :: mise en place de la prod pour sxa
    git-svn-id: https://dev.zuno.fr/zunoApp/zuno/Sxa/production@4354 06c77f61-839e-4d04-8f56-14a2b81da803

--- patch.sql
+++ patch.sql
@@ -1,12 +1,20 @@
+ 
 --
 -- Début du patch.sql généré par CompareSQL V1
+-- Thu May 13 01:59:03 +0200 2010
 --
+ 
 --
 -- Gestion des différences de structures de la BDD et des tables
 --
+ 
+ 
 --
 -- Ajout des données supplémentaires
 --
+ 
+ 
 --
 -- Suppression des données qui n'existent plus
 --
+ 
