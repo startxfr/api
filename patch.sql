f9755c495d4391923899d8d10632843939fef5aa
cl <cl@06c77f61-839e-4d04-8f56-14a2b81da803>
2010-06-01 13:35:48 +0000

    Zunodev :: correction de bug en provenance de zuno
    git-svn-id: https://dev.zuno.fr/zunoApp/zuno/Sxa/production@4500 06c77f61-839e-4d04-8f56-14a2b81da803

--- patch.sql
+++ patch.sql
@@ -1,7 +1,7 @@
  
 --
 -- Début du patch.sql généré par CompareSQL V1
--- Tue Jun 01 15:08:59 +0200 2010
+-- Tue Jun 01 15:35:14 +0200 2010
 --
  
 --
@@ -13,7 +13,7 @@
 -- Ajout des données supplémentaires
 --
  
-
+ 
 --
 -- Suppression des données qui n'existent plus
 --
