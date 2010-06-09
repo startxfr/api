011adfaa61cd25605f93cbcd1a3db66725a13816
cl <cl@06c77f61-839e-4d04-8f56-14a2b81da803>
2010-06-09 16:55:26 +0000

    Zunodev ::
    git-svn-id: https://dev.zuno.fr/zunoApp/zuno/Sxa/production@4847 06c77f61-839e-4d04-8f56-14a2b81da803

--- patch.sql
+++ patch.sql
@@ -1,20 +1,13 @@
  
 --
 -- Début du patch.sql généré par CompareSQL V1
--- Mon Jun 07 15:28:08 +0200 2010
+-- Wed Jun 09 18:53:42 +0200 2010
 --
  
 --
 -- Gestion des différences de structures de la BDD et des tables
 --
  
-ALTER TABLE `commande_produit` CHANGE `id_produit` `id_produit` varchar(32) NOT NULL DEFAULT '0';
-ALTER TABLE `devis_produit` CHANGE `id_produit` `id_produit` varchar(32) NOT NULL DEFAULT '0';
-ALTER TABLE `facture_produit` CHANGE `id_produit` `id_produit` varchar(32) NOT NULL DEFAULT '0';
-ALTER TABLE `produit` CHANGE `id_prod` `id_prod` varchar(32) NOT NULL DEFAULT '0';
-ALTER TABLE `produit_fournisseur` CHANGE `produit_id` `produit_id` varchar(32) NOT NULL DEFAULT '0';
-ALTER TABLE `projet` DROP `SSII_proj`;
-ALTER TABLE `projet` DROP `SSLL_proj`;
  
 --
 -- Ajout des données supplémentaires
@@ -24,3 +17,4 @@ ALTER TABLE `projet` DROP `SSLL_proj`;
 --
 -- Suppression des données qui n'existent plus
 --
+ 
