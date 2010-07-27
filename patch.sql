965e02931c9383ae2a81cbb30725a5397315b079
cl <cl@06c77f61-839e-4d04-8f56-14a2b81da803>
2010-07-27 19:17:46 +0000

    Zunodev :: mise a jour de la prod
    git-svn-id: https://dev.zuno.fr/zunoApp/zuno/Sxa/production@5268 06c77f61-839e-4d04-8f56-14a2b81da803

--- patch.sql
+++ patch.sql
@@ -1,18 +1,13 @@
  
 --
 -- Début du patch.sql généré par CompareSQL V1
--- Tue Jul 27 18:55:03 +0200 2010
+-- Tue Jul 27 21:16:04 +0200 2010
 --
  
 --
 -- Gestion des différences de structures de la BDD et des tables
 --
  
-ALTER TABLE `commande_produit` CHANGE `remise` `remise` decimal(5,2) NOT NULL DEFAULT '0.00';
-ALTER TABLE `commande_produit` CHANGE `remiseF` `remiseF` decimal(5,2) NOT NULL DEFAULT '0.00';
-ALTER TABLE `devis_produit` CHANGE `remise` `remise` decimal(5,2) NOT NULL DEFAULT '0.00';
-ALTER TABLE `entreprise` CHANGE `remise_ent` `remise_ent` decimal(5,2) DEFAULT '0.00';
-ALTER TABLE `facture_produit` CHANGE `remise` `remise` decimal(5,2) NOT NULL DEFAULT '0.00';
  
 --
 -- Ajout des données supplémentaires
