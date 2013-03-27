86248521b7a5d7ffc828b01c517d8c235272a79e
chris <cl@startx.fr>
2013-03-27 18:45:52 +0100

    adding an import script who update nosql backend with bson dumps located in api-lib/tmp.

---
+++ importDatabase.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/php
+<?php
+$localRootPath = '/var/www/virtual/api.startx.fr/api-lib/tmp/';
+$user = 'dev';
+$pass = 'dev';
+$db = 'sxapi';
+$collections = array(
+	'redirect.hit',
+	'sxapi.api',
+	'sxapi.application',
+	'sxapi.logs',
+	'sxapi.models',
+	'sxapi.ressources',
+	'sxapi.session',
+	'sxapi.users',
+	'system.indexes',
+	'system.users',
+);
+foreach($collections as $col) 
+shell_exec("mongoimport -d $db -c $col -u $user -p $pass $localRootPath/dump.$db-$col.bson");
+
+?>
