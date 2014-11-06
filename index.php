73caccfe33430a193cd0ebd3fbab3001f16d368a
startxfr <clarue@startx.fr>
2014-11-06 11:20:13 +0100

    dev commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require_once('api-lib' . DIRECTORY_SEPARATOR .'kernel' . DIRECTORY_SEPARATOR . 'loader.php');
 
 Api::$nosqlApiBackend = '{
-        "connection" : "mongodb://startx:314159@127.0.0.1:27017",
+        "connection" : "mongodb://dev:dev@127.0.0.1:27017",
         "base" : "sxapi",
         "api_collection" : "sxapi.api"
     }';
