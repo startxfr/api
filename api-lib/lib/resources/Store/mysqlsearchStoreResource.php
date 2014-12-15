eff994240a8f273497327cc91ab626aab45fdfa1
jrenouf <renoufjoseph@gmail.com>
2014-12-15 17:58:02 +0100

    dev2 commit

--- api-lib/lib/resources/Store/mysqlsearchStoreResource.php
+++ api-lib/lib/resources/Store/mysqlsearchStoreResource.php
@@ -32,12 +32,14 @@ class mysqlsearchStoreResource extends mysqlStoreResource implements IResource {
                 $limit = (int) $api->getInput()->getParam($this->getConfig('limitParam', '_limit'), $countResult);                
                 $customQuery = $this->composeSearchQuery( $search, $start, $limit );
                 $data = $store->execQuery($customQuery);      
-                var_dump($data);
-                exit(0);
-                $return = $this->filterResults($data);                                
+//                var_dump($data);
+//                exit(0);                
+                $return = $this->filterResults($data);
+                $size = count($return);
+                $nRet = array_slice($return, $start, $limit);
                 $message = sprintf($this->getConfig('message_service_read', 'message service read'), count($return), $countResult, session_id());
                 $api->logInfo(910, "'" . __FUNCTION__ . "' in '" . get_class($this) . "' return : " . $message, $this->getResourceTrace(__FUNCTION__, false), 1);                                
-                return array(true, $message, $return, $countResult);                                                                                                                       
+                return array(true, $message, $nRet, $size);                                                                                                                     
             }
             else
                 return parent::readAction();
@@ -48,9 +50,8 @@ class mysqlsearchStoreResource extends mysqlStoreResource implements IResource {
         return true;
     }
 
-    private function composeSearchQuery( $search , $start, $limit ) {
-        $lim = " LIMIT $start, $limit ";
-        $join = "SELECT * "                                
+    private function composeSearchQuery( $search ) {
+        $join = "SELECT f.* "                                
                 . "FROM formation_session AS f "
                 . "LEFT JOIN formation_cours AS cours ON cours.name_cours = f.cours_id_session "
                 . "LEFT JOIN formation_centre AS centre ON centre.id_centre = f.location_id_session "
@@ -62,20 +63,24 @@ class mysqlsearchStoreResource extends mysqlStoreResource implements IResource {
                 . "LEFT JOIN entreprise ON entreprise.id_ent = cc.entreprise_cont "
                 ;         
         $where = "WHERE ";
-        var_dump($search);
+//        var_dump('compose query start');
+//        var_dump($search);
         $s1 = preg_replace(array('/ AND | ET /i', '/ OR | OU /i'), array('&&', '||'), $search);
-        $s2 = preg_replace_callback("/[A-Za-z0-9 _-]*:[A-Za-z0-9 _-]*/", "self::convertTest", $s1);
+        $s2 = preg_replace_callback("/([^&|:()]*):([^&|:()]*)/", "self::convertTest", $s1);
         if ( $s2 === $search )
-            $s2 = " f.* LIKE %$search% ";
-        var_dump($s2);
-        exit(0);
-        $query = $join . $where . $s2 . $lim;
+            $s2 = " f.* LIKE '%$search%' ";
+//        var_dump($s2);
+//        var_dump('compose query end');
+//        exit(0);
+        $query = $join . $where . $s2 . ';';
         return $query;
     }          
     
     private function convertTest( $str ) {
-        var_dump($str);
-        list($tag, $arg) = array_map('trim', explode(":", $str[0]));
+//        var_dump('convert test start');
+//        var_dump($str);
+        #list($tag, $arg) = array_map('trim', explode(":", $str[0]));
+        list(,$tag, $arg) = array_map('trim', $str);
         switch ( (string) $tag ) 
         {
             case "time":
@@ -84,44 +89,102 @@ class mysqlsearchStoreResource extends mysqlStoreResource implements IResource {
                 break;
             case "training":
             case "cours":
-                $s =  " ( cours.name_cours LIKE %$arg% OR cours.title_cours LIKE %$arg% ) ";                
+                $s =  " ( cours.name_cours LIKE '%$arg%' OR cours.title_cours LIKE '%$arg%' ) ";                
                 break;
             case "courses" :
             case "cursus":
-                $s =  " ( cursus.name_cursus LIKE %$arg% OR cursus.title_cursus LIKE %$arg% ) ";
+                $s =  " ( cursus.name_cursus LIKE '%$arg%' OR cursus.title_cursus LIKE '%$arg%' ) ";
                 break;
             case "partner" :
             case "partenaire":
-                $s =  " ( partenaire.name_part LIKE %$arg% OR partenaire.title_part LIKE %$arg% ) ";
+                $s =  " ( partenaire.name_part LIKE '%$arg%' OR partenaire.title_part LIKE '%$arg%' ) ";
                 break;
             case "trainer" :
             case "formateur":
-                $s =  " ( formateur.initial_formateur LIKE %$arg% OR cf.nom_cont LIKE %$arg% OR cf.prenom_cont LIKE %$arg% ) ";
+                $s =  " ( formateur.initial_formateur LIKE '%$arg%' OR cf.nom_cont LIKE '%$arg%' OR cf.prenom_cont LIKE '%$arg%' ) ";
                 break;
             case "location" :
             case "lieux":
-                $s =  " ( cc.add1_cont LIKE %$arg% OR cc.add2_cont LIKE %$arg% OR cc.cp_cont LIKE %$arg% OR cc.ville_cont LIKE %$arg% ) ";
+                $s =  " ( cc.add1_cont LIKE '%$arg%' OR cc.add2_cont LIKE '%$arg%' OR cc.cp_cont LIKE '%$arg%' OR cc.ville_cont LIKE '%$arg%' ) ";
                 break;
             default:
                 $s =  " ";
                 break;
         }
+//        var_dump('convert test end');
         return $s;
     }
+      
     
-    private function getTimeWhere($arg) {
-        if (preg_match("/\d{4}/", $arg)) {
-            
+    private function getTimeWhere( $date )
+    {
+//        var_dump('getTimeWhere start');   
+	$d2 = "([12][0-9]{3}|[0-1]?[0-9]\/[12][0-9]{3}|[0-3]?[0-9]\/[0-1]?[0-9]\/[12][0-9]{3})";
+	if (preg_match("/^$d2$/", $date, $o)) {
+//            var_dump('d');
+//            var_dump($o);
+            $gap = $this->getTime($o[1], true);
+//            var_dump($gap);
+            $q = " ( f.start_session BETWEEN " . $gap[0] . " AND " . $gap[1] . " ) ";
+	}
+        else if (preg_match("/^$d2\s+-$/", $date, $o)) {
+//            var_dump('d -');
+//            var_dump($o);  
+            $q = " ( f.start_session >= " . $this->getTime($o[1]) . " ) ";      
         }
-        else if (preg_match("/\d{2}\/\d{2}/", $arg)) {
-            
+        else if (preg_match("/^-\s+$d2$/", $date, $o)) {
+//            var_dump('- d');
+//            var_dump($o);
+            $q = " ( f.start_session <= " . $this->getTime($o[1]) . " ) ";
         }
-        else if (preg_match("/(\d{2}\/\d{2}\/\d{2}( )*-)||(\d{2}\/\d{2}( )*-)||(\d{4}( )*-)/", $arg)) {
-            
+        else if (preg_match("/^$d2\s+-\s+$d2$/", $date, $o)) {
+//            var_dump('d1 - d2');
+//            var_dump($o);
+            $q = " ( f.start_session BETWEEN " . $this->getTime($o[1]) . " AND " . $this->getTime($o[2]) . " ) ";
         }
-        else if (preg_match("/(-*( )\d{2}\/\d{2}\/\d{2})||(-*( )\d{2}\/\d{2})||(-*( )\d{4})/", $arg)) {
-            
+//        var_dump('getTimeWhere end');
+	return $q;
+    }
+    
+    private function getTime( $date, $gap = false ) {
+        $t = explode("/", $date);
+        date_default_timezone_set('Europe/Berlin');
+        switch(count($t)) {
+            case 1:
+                echo "1/1/".$t[0].PHP_EOL;
+                if ($gap) {
+                    $time = [];
+                    $time[] = strtotime("1/1/".$t[0]);
+                    $time[] = strtotime("1/1/".($t[0] + 1));
+                }
+                else
+                    $time = strtotime("1/1/".$t[0]);
+                break;
+            case 2:
+                echo "1/".$t[0]."/".$t[1].PHP_EOL;
+                if ($gap) {
+                    $time = [];
+                    $time[] = strtotime("1/".$t[0]."/".$t[1]);
+                    $time[] = strtotime("1/". ($t[0] + 1) ."/".$t[1]);
+                }
+                else
+                    $time = strtotime("1/".$t[0]."/".$t[1]);
+                break;
+            case 3:
+                echo $t[1]."/".$t[0]."/".$t[2].PHP_EOL;
+                if ($gap) {
+                    $time = [];
+                    $time[] = strtotime($t[1]."/".$t[0]."/".$t[2]);
+                    $time[] = strtotime(($t[1] + 1) ."/".$t[0]."/".$t[2]);
+                }
+                else
+                    $time = strtotime($t[1]."/".$t[0]."/".$t[2]);
+                break;
+            default:
+                $time = 0;
+                break;
         }
+        return $time;
     }
     
 }
